4cfc5a79fd831d3feb5a63b1a3b63faed9bb8dd7
Sumon SN <sumonsn117@gmail.com>
2021-02-15 11:31:11 +0600

    update wapi

--- wapi/get_address.py
+++ wapi/get_address.py
@@ -1,12 +1,32 @@
 import json
-from wapi import "
+from wapi import *
+def doge_address():
+                 data = json.dumps({'api_key' = "api_key",
+                                    'currency' = "DOGE"
+                                    'username' = "username"
+                                    'action' = 'getnewaddress'
+                                    })
+                 r = requests.post( wapi_key, data = data)
 
-data = json.dumps({'api_key' = "api_key",
-                   'currency' = "currency",
-                   'username' = "username"
-                   'action' = 'getnewaddress'
-                   })
-r = requests.post( wapi_key, data = data)
+                 js = json.loads(r.text)
+                 return js['address']
+def ltc_address():
+                data = json.dumps({'api_key' = "api_key",
+                                    'currency' = "LTC"
+                                    'username' = "username"
+                                    'action' = 'getnewaddress'
+                                    })
+                 r = requests.post( wapi_key, data = data)
 
-js = json.loads(r.text)
-return js['address']
+                 js = json.loads(r.text)
+                 return js['address']
+def btc_address():
+                data = json.dumps({'api_key' = "api_key",
+                                    'currency' = "BTC"
+                                    'username' = "username"
+                                    'action' = 'getnewaddress'
+                                    })
+                 r = requests.post( wapi_key, data = data)
+
+                 js = json.loads(r.text)
+                 return js['address']
--- wapi/wapi.py
+++ wapi/wapi.py
@@ -2,5 +2,4 @@ from config import *
 
 wapi_url = "https://mrcyjanek.net/wapi/api.php"
 api_key = API_KEY
-currency = str(CURRENCY.upper())
 username = str(USERNAME)
